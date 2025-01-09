898dadba09129888fc0cb38e8e81793b848bc3f1
Garrett <52760019+Casimir255@users.noreply.github.com>
2025-01-08 22:25:37 -0600

    Another typo

--- ModAPI/NexusAPI.cs
+++ ModAPI/NexusAPI.cs
@@ -241,6 +241,7 @@ namespace NexusModAPI
             GetAllOnlineServers,
             GetAllOnlinePlayers,
             SendChatToDiscord,
+            SendChatToServers,
             RemoveSpawnPadActivation
         }
 
